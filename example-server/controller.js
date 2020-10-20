5080e6d70a5a1dfd85ec2910214884b77c6a67ec
Pär Strindevall <par@strindevall.com>
2020-10-20 20:35:57 +0200

    Update controller with more appropriate HTTP responses

--- example-server/controller.js
+++ example-server/controller.js
@@ -5,34 +5,69 @@ const River = mongoose.model('River');
 
 exports.get = function(request, response) {
     River.findById(request.params.riverId, function(error, river) {
-        if (error)
-            response.send(error);
-        response.json(river);
+        if (error) {
+            response.status(400).send(error)
+            return
+        }
+        
+        if (!river) {
+            response.status(404).send()
+            return
+        }
+
+        response.json(river)
     });
 };
 
 exports.publish = function(request, response) {
     const river = new River(request.body);
     river.save(function(error, river) {
-        if (error)
-            response.send(error);
-        response.json(river);
+        if (error) {
+            response.status(400).send(error)
+            return
+        }
+
+        response.json(river)
     });
 };
 
 
 exports.update = function(request, response) {
-    River.findOneAndUpdate({ _id: request.params.riverId }, request.body, { new: true }, function(error, river) {
-        if (error)
-            response.send(error);
-        response.json(river);
-    });
-};
+    const updatedRiver = new River(request.body)
+    updatedRiver.validate().then(() => {
+        River.findOneAndUpdate({ _id: request.params.riverId }, request.body, { new: true }, function(error, river) {
+            if (error) {
+                response.status(400).send(error)
+                return
+            }
+    
+            if (!river) {
+                response.status(404).send()
+                return
+            }
+    
+            response.json(river)
+        });
+    })
+    .catch(error => {
+        response.status(400).send(error)
+    })
+}
 
 exports.delete = function(request, response) {
-    River.deleteOne({ _id: request.params.riverId }, function(error, river) {
-        if (error)
-            response.send(error);
-        response.json({ message: 'River successfully deleted' });
+    River.deleteOne({ _id: request.params.riverId }, function(error, result) {
+        if (error) {
+            response.status(400).send(error)
+            return
+        }
+
+        console.log(result)
+            
+        if (result.deletedCount == 0) {
+            response.status(204).send()
+            return
+        }
+
+        response.status(200).send()
   });
 };
