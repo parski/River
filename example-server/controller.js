69950746cb5cf2a3ca404a7e527e5e45ea5626b2
Pär Strindevall <par@strindevall.com>
2020-10-20 17:34:17 +0200

    Use more verbose variable names in controller

--- example-server/controller.js
+++ example-server/controller.js
@@ -3,36 +3,36 @@
 const mongoose = require('mongoose');
 const River = mongoose.model('River');
 
-exports.get = function(req, res) {
-    River.findById(req.params.riverId, function(err, river) {
-        if (err)
-            res.send(err);
-        res.json(river);
+exports.get = function(request, response) {
+    River.findById(request.params.riverId, function(error, river) {
+        if (error)
+            response.send(error);
+        response.json(river);
     });
 };
 
-exports.publish = function(req, res) {
-    const river = new River(req.body);
-    river.save(function(err, river) {
-        if (err)
-            res.send(err);
-        res.json(river);
+exports.publish = function(request, response) {
+    const river = new River(request.body);
+    river.save(function(error, river) {
+        if (error)
+            response.send(error);
+        response.json(river);
     });
 };
 
 
-exports.update = function(req, res) {
-    River.findOneAndUpdate({ _id: req.params.riverId }, req.body, { new: true }, function(err, river) {
-        if (err)
-            res.send(err);
-        res.json(river);
+exports.update = function(request, response) {
+    River.findOneAndUpdate({ _id: request.params.riverId }, request.body, { new: true }, function(error, river) {
+        if (error)
+            response.send(error);
+        response.json(river);
     });
 };
 
-exports.delete = function(req, res) {
-    River.remove({ _id: req.params.riverId }, function(err, river) {
-        if (err)
-            res.send(err);
-        res.json({ message: 'River successfully deleted' });
+exports.delete = function(request, response) {
+    River.remove({ _id: request.params.riverId }, function(error, river) {
+        if (error)
+            response.send(error);
+        response.json({ message: 'River successfully deleted' });
   });
 };
